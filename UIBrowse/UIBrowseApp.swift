bcedfc76c36d8e5df65ecc261a58e88920885cba
jiun-dev <jiun.dev@gmail.com>
2021-05-23 13:22:00 +0900

    Initial Commit

---
+++ UIBrowse/UIBrowseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIBrowseApp.swift
+//  UIBrowse
+//
+//  Created by shinjiun on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct UIBrowseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
